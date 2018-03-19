c47c5618ccddf1753d1cc7c11b31cd97ac810331
Zaheer <zaheer.dollie@ipreo.com>
2018-03-19 21:03:19 +0200

    svg practice 1

---
+++ 01/main.js
@@ -0,0 +1,4 @@
+var svg = d3.select("#canvas").append("svg").attr("width", 500 ).attr("height", 400);
+svg.append("rect").attr("width", 200).attr("height", 100).attr("fill", "yellow");
+svg.append("circle").attr("cx", 150).attr("cy", 150).attr("r", 25).attr("fill", "red");
+svg.append("line").attr("x1", 180).attr("y1", 250).attr("x2", 250).attr("y2", 280).attr("stroke", "blue").attr("stroke-width", 5);
